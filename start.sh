6735cab37368aa37f8ee8ae3055672898da039aa
Harry Hawkesford <harryhawkesford@mac.home>
2026-02-15 11:25:10 +0000

    Fresh starts ahead

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Start the docker-compose services
-docker-compose up -d
-
-echo "dog-walking is running at http://localhost:3000"
--- stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Stop and remove the dog-walking container if running
-docker-compose down
-
-echo "Stopped and removed dog-walking container."
